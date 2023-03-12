8b94f96ecc5847c3d9817c171f10a931f0e7f1d6
mark.baranov <mark.baranov@snowball.dev>
2023-03-12 21:28:19 +0800

    feat(types): service interface

--- simple_crud/types/types.go
+++ simple_crud/types/types.go
@@ -5,3 +5,11 @@ type WeatherCondition struct {
 	Temperature float64
 	WindSpeed   float64
 }
+
+type Service interface {
+	CreateWeatherCondition(temperature float64, windSpeed float64) (*WeatherCondition, error)
+	DeleteWeatherCondition(id string) error
+	UpdateWeatherCondition(id string, temperature *float64, windSpeed *float64) error
+	ReadWeatherCondition(id string) (*WeatherCondition, error)
+	ListWeatherConditions() ([]*WeatherCondition, error)
+}

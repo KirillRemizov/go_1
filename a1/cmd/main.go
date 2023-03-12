9ba7c51274ad7fe3be81c980239f59d15d9f3ee8
mark.baranov <mark.baranov@snowball.dev>
2023-03-12 12:32:19 +0800

    feat(main): update

--- a1/cmd/main.go
+++ a1/cmd/main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello GO!")
+
+	a, err := firstFunc("a", 2)
+	if err != nil {
+		// TODO: handle err
+	}
+
+	a = 5
+
+	fmt.Println(a)
+
+}
+
+func firstFunc(in string, in2 int) (a int, err error) {
+	return 1, nil
 }
